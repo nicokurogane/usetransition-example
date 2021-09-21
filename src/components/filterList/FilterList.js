95965cc11882da304e7f9e0bc7aa423e248289be
Nicolas Martinez <fyedream@gmail.com>
2021-09-21 07:58:23 -0600

    ✨ Adds a grey overlay while react is rendering new state

--- src/components/filterList/FilterList.js
+++ src/components/filterList/FilterList.js
@@ -4,28 +4,24 @@ import ListItem from "../listItem/ListItem";
 
 const FilterList = ({ names = [] }) => {
   const [query, setQuery] = useState(""); //this is what the user inputs
-
-  // TODO 2- Identify those updates that will be delayed by startTransition function
   const [highlight, setHighLight] = useState(""); //this state update that will be delayed;
 
-  // TODO 3- declare the hook: as useState, the function is the second deconstructed parameter.
+  // TODO 2- declare the hook: as useState, the isPending is the first deconstructed parameter.
   const [isPending, startTransition] = useTransition();
 
   const changeHandler = ({ target: { value } }) => {
     setQuery(value);
-    // TODO 4- Pass as a callback the pieces you already identified
     startTransition(() => setHighLight(value));
   };
 
   return (
     <div>
       <input onChange={changeHandler} value={query} type="text" />
-      {
-        // TODO 5- Enjoy :D
-      }
-      {names.map((name, i) => (
-        <ListItem key={i} name={name} highlight={highlight} />
-      ))}
+      <div className={isPending ? "rendering-data" : ""}>
+        {names.map((name, i) => (
+          <ListItem key={i} name={name} highlight={highlight} />
+        ))}
+      </div>
     </div>
   );
 };
