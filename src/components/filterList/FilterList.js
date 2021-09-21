61e5073ed8b0e9b3d25797152bf7994f04461fb7
Nicolas Martinez <fyedream@gmail.com>
2021-09-21 07:10:06 -0600

    ✨ Adds useTransition to improve performance

--- src/components/filterList/FilterList.js
+++ src/components/filterList/FilterList.js
@@ -1,16 +1,30 @@
-import { useState } from "react";
+// TODO 1 - Import useTransition
+import { useState, useTransition } from "react";
 import ListItem from "../listItem/ListItem";
 
-const FilterList = ({ names=[] }) => {
-  const [query, setQuery] = useState("");
+const FilterList = ({ names = [] }) => {
+  const [query, setQuery] = useState(""); //this is what the user inputs
 
-  const changeHandler = ({ target: { value } }) => setQuery(value);
+  // TODO 2- Identify those updates that will be delayed by startTransition function
+  const [highlight, setHighLight] = useState(""); //this state update that will be delayed;
+
+  // TODO 3- declare the hook: as useState, the function is the second deconstructed parameter.
+  const [isPending, startTransition] = useTransition();
+
+  const changeHandler = ({ target: { value } }) => {
+    setQuery(value);
+    // TODO 4- Pass as a callback the pieces you already identified
+    startTransition(() => setHighLight(value));
+  };
 
   return (
     <div>
       <input onChange={changeHandler} value={query} type="text" />
+      {
+        // TODO 5- Enjoy :D
+      }
       {names.map((name, i) => (
-        <ListItem key={i} name={name} highlight={query} />
+        <ListItem key={i} name={name} highlight={highlight} />
       ))}
     </div>
   );
